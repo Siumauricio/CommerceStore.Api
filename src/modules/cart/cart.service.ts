f3bcff0540ec9602b13486cdbc35d729b33477d8
Mauricio <Siumauricio@unitec.edu>
2022-05-20 13:49:30 -0600

    feat: add a method findAll to get all products in a cart

--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ApiResponse } from '../../common/config/apiresponse';
+import { Cart } from '../../repository/entities/cart.entity';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(
+    @InjectRepository(Cart)
+    private productsRepository: Repository<Cart>,
+  ) {}
+
+  async findAll(idUser: string): Promise<ApiResponse<Cart[]>> {
+    const cart = await this.productsRepository.find({
+      where: { idUser },
+    });
+    return ApiResponse(HttpStatus.OK, 'Carts retrieved successfully', cart);
+  }
+}
