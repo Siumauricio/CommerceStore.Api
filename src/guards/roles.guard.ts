f090c752768e290518403e1dc22b2c026c06284a
Mauricio <Siumauricio@unitec.edu>
2022-05-20 13:48:29 -0600

    refactor: add roles guard for authentication graphql

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 import { Role } from '../modules/auth/roles.enum';
 import { Users } from '../repository/entities/users.entity';
@@ -13,9 +14,16 @@ export class RolesGuard implements CanActivate {
       ROLES_KEY,
       [context.getHandler(), context.getClass()],
     );
+
     if (!hasRequiredRoles) {
       return true;
     }
+    const ctx = GqlExecutionContext.create(context);
+    if (ctx.getType() === 'graphql') {
+      const user: Users = ctx.getContext().req.user;
+      return hasRequiredRoles.some((role) => user.userType.includes(role));
+    }
+
     const { user: contextUser } = context.switchToHttp().getRequest();
     const user: Users = contextUser;
     return hasRequiredRoles.some((role) => user.userType.includes(role));
