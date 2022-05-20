8c51d9e3879b75cc9fe047228b05ac08e9713ddd
Mauricio <Siumauricio@unitec.edu>
2022-05-20 13:47:06 -0600

    feat: added generic apiresponse for graphql type

--- src/common/config/apiresponse.ts
+++ src/common/config/apiresponse.ts
@@ -1,4 +1,6 @@
-import { HttpStatus } from '@nestjs/common';
+import { isArray } from 'class-validator';
+import { HttpStatus, Type } from '@nestjs/common';
+import { Field, ObjectType } from '@nestjs/graphql';
 
 export interface ApiResponse<T> {
   statusCode: HttpStatus;
@@ -12,3 +14,32 @@ export const ApiResponse = <T>(
 ) => {
   return { statusCode, message, data };
 };
+
+export function QueryResponse<T>(ItemType: Type<T>): any {
+  @ObjectType({ isAbstract: true })
+  abstract class QueryResponse {
+    @Field({ nullable: false })
+    statusCode?: HttpStatus;
+
+    @Field({ nullable: false })
+    message?: string;
+
+    @Field(() => ItemType, { nullable: true })
+    data?: T;
+  }
+  return QueryResponse;
+}
+export function QueryResponseArray<T>(ItemType: Type<T>): any {
+  @ObjectType({ isAbstract: true })
+  abstract class QueryResponseArray {
+    @Field({ nullable: false })
+    statusCode?: HttpStatus;
+
+    @Field({ nullable: false })
+    message?: string;
+
+    @Field(() => [ItemType], { nullable: true })
+    data?: T[];
+  }
+  return QueryResponseArray;
+}
